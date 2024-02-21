91d75e743c4f56a92545c9af76af08299c3aa89d
linnefromice <linnefromice@gmail.com>
2024-02-21 20:57:54 +0900

    update

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,17 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+pub fn sub(left: usize, right: usize) -> usize {
+    left - right
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        assert_eq!(add(2, 2), 4);
+        assert_eq!(sub(3, 1), 2);
     }
 }
