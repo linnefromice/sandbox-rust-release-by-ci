4dad86210f0c7b181c35df297136c39fa34fdb7c
linnefromice <linnefromice@gmail.com>
2024-02-22 11:04:01 +0900

    update: logics

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,10 @@ pub fn sub(left: usize, right: usize) -> usize {
     left - right
 }
 
+pub fn mul(left: usize, right: usize) -> usize {
+    left * right
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -20,5 +24,11 @@ mod tests {
     fn test_sub() {
         assert_eq!(sub(2, 2), 0);
         assert_eq!(sub(4, 3), 1);
+    }
+
+    #[test]
+    fn test_mul() {
+        assert_eq!(mul(2, 2), 4);
+        assert_eq!(mul(4, 3), 12);
     }
 }
